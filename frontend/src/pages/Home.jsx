52b0dafc897ee02b1a658b1a4a5d2264224e7049
Tefin bose <tefinbose2003@gmail.com>
2026-02-03 15:17:15 +0530

    Add View and Category components to Home page layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Add from '../components/Add'
 import { Link } from 'react-router-dom'
 import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-FontAwesomeIcon
+import Category from '../components/Category'
+import View from '../components/View'
 function Home() {
   return (
 <>
@@ -11,6 +12,17 @@ function Home() {
 <div className="container d-flex justify-content-between mt-4">
   <Add/>
   <h6><Link  style={{textDecoration:"none"}} to={"/watchhistory"}><p > Watch-history <FontAwesomeIcon icon={faClockRotateLeft} /></p></Link></h6>
+</div>
+<div className="container-fluid">
+  <div className="row">
+    <div className="col-md-9">
+      <View/>
+    </div>
+    <div className="col-md-3">
+      <Category/>
+    </div>
+    
+  </div>
 </div>
 </>
 

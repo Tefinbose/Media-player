b33f77eab29d2105762b393638790f52de409e54
Tefin bose <tefinbose2003@gmail.com>
2026-03-25 15:57:03 +0530

    feat: set up json-server with initial db.json and server configuration

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
- 
+//  import json-server
+const jsonserver = require('json-server');
+// create server
+const mediaplayerserver = jsonserver.create()
+// create a middleware to parse the json data
+const middleware = jsonserver.defaults()
+// path to store the data
+const routes = jsonserver.router('db.json')
+
+// Tell the server to use the middleware and routes
+mediaplayerserver.use(middleware)
+mediaplayerserver.use(routes)
+// set a port 
+PORT = 4000||process.env.PORT   //This chooses the available port
+// run the server
+mediaplayerserver.listen(PORT,()=>{
+   console.log(`server running successfully at the port number ${PORT}`);
+})
